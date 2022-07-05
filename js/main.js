e49dc95e587100fbb6a89c64469996c39b193b0a
coleochieng <nicole.ochieng98@gmail.com>
2022-07-05 02:17:40 -0400

    adding JS

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+let board; // will be a row*column-type nested array
+let turn; // 1 or -1; 0 for no checker in that cell
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+init();
+
+//initialize state, then call render()
+function init() {
+ board = [
+   [0, 0, 0, 0], // column 0
+   [0, 0, 0, 0], // column 1
+   [0, 0, 0, 0], // column 2
+   [0, 0, 0, 0], // column 3
+   [0, 0, 0, 0], // column 4
+ ]; 
+  turn = 1;
+ //render(); 
+}
