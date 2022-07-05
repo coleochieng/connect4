6aef2d6546349ee443ffbe85edec24acf37fe0e8
coleochieng <nicole.ochieng98@gmail.com>
2022-07-05 19:58:19 -0400

    test

--- js/main.js
+++ js/main.js
@@ -18,12 +18,14 @@ init();
 //initialize state, then call render()
 function init() {
  board = [
-   [0, 0, 0, 0], // column 0
-   [0, 0, 0, 0], // column 1
-   [0, 0, 0, 0], // column 2
-   [0, 0, 0, 0], // column 3
-   [0, 0, 0, 0], // column 4
+   [0, 0, 0, 0, 0, 0], // column 0
+   [0, 0, 0, 0, 0, 0], // column 1
+   [0, 0, 0, 0, 0, 0], // column 2
+   [0, 0, 0, 0, 0, 0], // column 3
+   [0, 0, 0, 0, 0, 0], // column 4 
+   [0, 0, 0, 0, 0, 0], // column 5 
+   [0, 0, 0, 0, 0, 0], // column 6
  ]; 
   turn = 1;
- //render(); 
+ // render(); 
 }
